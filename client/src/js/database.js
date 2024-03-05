56614b6ac6309de5eb7e70b437b50af38ed4d1c0
Brendan McGinty <brendan.mcginty14@gmail.com>
2024-03-05 15:00:02 -0500

    added the initial logic in database js for the put route

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  console.log('Successfully put to the database!');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', "readwrite") // Making sure we can have read / write access
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content});
+  const results = await request;
+
+  console.log('- data has been saved to the database!', result.value)
+  console.error('putDb not implemented')
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
